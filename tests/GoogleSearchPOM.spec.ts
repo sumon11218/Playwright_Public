c23f29fbe12a400dbdd20b4b73a33fa328ecb7ed
Sumon Kashem <sumon.kashem@vroom.com>
2023-03-14 09:35:47 -0400

    test

--- tests/GoogleSearchPOM.spec.ts
+++ tests/GoogleSearchPOM.spec.ts
@@ -23,7 +23,7 @@ for(let suite of combined){
                 it's printing the number for
             */
              await googleSearchResults.getSearchNumber(userValue)
-             //testing one three
+             //testing one
         })//end of test
     }//end of sub loop
 }//end of main loop
